65ce877d5494a76ad16c8c00b58268e10d403a4e
IRFAN <mmirfan2232@gmail.com>
2026-07-29 21:41:03 +0500

    profile managment

--- client/src/Profile.jsx
+++ client/src/Profile.jsx
@@ -1,16 +1,24 @@
 import React , {useState} from "react";
 function Profile(){
+    const [profilePic , setProfilePic] = useState(null);
+    function handleChange(e){
+        const file = e.target.files[0]
+        if(file){
+            setProfilePic(URL.createObjectURL(file))
+        }
+    }
  return(
-    <div>
-        <form action="">
-            <label htmlFor="name">name</label>
-            <input type="text"  />
-            <label htmlFor="pic"></label>
-
-        </form>
-        <p className="pic"></p>
-        <p className="profile_name"></p>
-    </div>
+   <div style={{ textAlign: "center", marginTop: "50px" }}>
+    <img src={profilePic || "https://via.placeholder.com/150" }alt="profile pic"     style={{
+          width: "150px",
+          height: "150px",
+          borderRadius: "50%",
+          objectFit: "cover",
+          border: "2px solid gray",
+        }} />
+    <p>select profile picture</p>
+    <input type="file" accept="image/" onChange={handleChange} />
+   </div>
  )   
 }
 export default Profile;
